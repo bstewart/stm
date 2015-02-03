518ab4003049a1294c1a02ed8403a56d3b7559ab
Amos Elberg <amos.elberg@gmail.com>
2015-02-03 16:43:54 -0500

    Attempt to fix optimization issue

--- R/STMestep.R
+++ R/STMestep.R
@@ -65,9 +65,11 @@ estep <- function(documents, beta.index, update.mu, #null allows for intercept o
     betaexpeta_colsums <- 0
     betaexpeta <- 0
     sumexpeta <- 0
+    global_eta <- 0
     eta <- optim(par=init, 
                        fn=function(eta) {
                          # {\sum_{v=1}^V c_v log [\sum_k beta_{k,v} exp(eta_k)] }- Wlog \sum exp(eta_k)
+                         global_eta <<- eta
                          expeta <<- c(exp(eta),1)
                          sumexpeta <<- sum(expeta)
                          betaexpeta <<- beta.i * expeta
@@ -81,57 +83,67 @@ estep <- function(documents, beta.index, update.mu, #null allows for intercept o
                          part2 <- .5*sum(diff*siginvdiff)
                          part2 - part1  
                        },  gr=function(eta) {
-                         denom <- doc.ct/betaexpeta_colsums
-                         part1 <- (betaexpeta%*%denom)[-length(expeta)] - expeta[1]*(Ndoc/sumexpeta)  
-                         as.numeric(siginvdiff - part1)
+                         if (identical(eta,global_eta)) {
+                           denom <- doc.ct/betaexpeta_colsums
+                           part1 <- (betaexpeta%*%denom)[-length(expeta)] - expeta[1]*(Ndoc/sumexpeta)  
+                           as.numeric(siginvdiff - part1)
+                         } else {
+                           expeta.sh <- exp(eta) 
+                           expeta <- c(expeta.sh,1)
+                           Ez <- expeta*beta
+                           denom <- doc.ct/.colSums(Ez, nrow(Ez), ncol(Ez))
+                           part1 <- (Ez%*%denom)[-length(expeta)] - expeta.sh*(Ndoc/sum(expeta))  
+                           part2 <- siginv%*%(eta-mu) 
+                           as.numeric(part2 - part1)                           
+                         }
                        },
                        method="BFGS", 
                        control=list(maxit=500)
     )$par
     
-    doc.results <- logisticnormal(eta=init, mu=mu.i, siginv=siginv, beta=beta.i, 
-                                 doc=doc, sigmaentropy=sigmaentropy)
+#     doc.results <- logisticnormal(eta=init, mu=mu.i, siginv=siginv, beta=beta.i, 
+#                                  doc=doc, sigmaentropy=sigmaentropy)
     
-#     #Solve for Hessian/Phi/Bound returning the result
-#       theta <- expeta/sumexpeta
-#   
-#    #pieces for the derivatives of the exp(eta)beta part
-#        EB <- t(betaexpeta)/betaexpeta_colsums #transpose and norm by (now) the row
-#   
-#    #at this point EB is the phi matrix
-#       phi <- EB*(doc.ct) #multiply through by word count
-#       phisums <- colSums(phi)
-#       phi <- t(phi) #transpose so its in the K by W format expected
-#       EB <- EB*sqrt(doc.ct) #set up matrix to take the cross product
-#     
-#     #First piece is the quotient rule portion that shows up from E[z], second piece is the part
-#     # that shows up regardless as in Wang and Blei (2013) for example.  Last element is just siginv
-#      hess <- -((diag(phisums) - crossprod(EB)) - 
-#                  Ndoc*(diag(theta) - theta%o%theta))[1:length(eta),1:length(eta)] + siginv
-#     
-#     ###
-#     # Bound
-#     
-#     nu <- try(chol2inv(chol.default(hess)), silent=TRUE)
-#     if(class(nu)=="try-error") {
-#       #brute force solve
-#       nu <- solve(hess)
-#       #only if we would produce negative variances do we bother doing nearPD
-#       if(any(diag(nu)<0)) nu <- as.matrix(nearPD(nu)$mat)
-#     }
-#      logphinorm <- log(colSums(theta*beta.i))
-#      part1 <- sum(doc.ct*logphinorm)
-# 
-#     bound[i] <- as.numeric(part1 + .5*determinant(nu, logarithm=TRUE)$modulus -
-#       .5*sum(diff*crossprod(diff,siginv)) - sigmaentropy)
-#     
-#     # done inferring the document
+    #Solve for Hessian/Phi/Bound returning the result
+      theta <- expeta/sumexpeta
+  
+   #pieces for the derivatives of the exp(eta)beta part
+       EB <- t(betaexpeta)/betaexpeta_colsums #transpose and norm by (now) the row
+  
+   #at this point EB is the phi matrix
+      phi <- EB*(doc.ct) #multiply through by word count
+      phisums <- colSums(phi)
+      phi <- t(phi) #transpose so its in the K by W format expected
+      EB <- EB*sqrt(doc.ct) #set up matrix to take the cross product
+    
+    #First piece is the quotient rule portion that shows up from E[z], second piece is the part
+    # that shows up regardless as in Wang and Blei (2013) for example.  Last element is just siginv
+     hess <- -((diag(phisums) - crossprod(EB)) - 
+                 Ndoc*(diag(theta) - theta%o%theta))[1:length(eta),1:length(eta)] + siginv
+    
+    ###
+    # Bound
+    
+    nu <- try(chol2inv(chol.default(hess)), silent=TRUE)
+    if(class(nu)=="try-error") {
+      #brute force solve
+      nu <- solve(hess)
+      #only if we would produce negative variances do we bother doing nearPD
+      if(any(diag(nu)<0)) nu <- as.matrix(nearPD(nu)$mat)
+    }
+     logphinorm <- log(colSums(theta*beta.i))
+     part1 <- sum(doc.ct*logphinorm)
+
+    bound[i] <- as.numeric(part1 + .5*determinant(nu, logarithm=TRUE)$modulus -
+      .5*sum(diff*crossprod(diff,siginv)) - sigmaentropy)
+    
+    # done inferring the document
 
     # update sufficient statistics 
-    sigma.ss <- sigma.ss + doc.results$eta$nu
-    beta.ss[[aspect]][,words] <- doc.results$phis + beta.ss[[aspect]][,words]
-    bound[i] <- doc.results$bound
-    lambda[[i]] <- doc.results$eta$lambda
+    sigma.ss <- sigma.ss + nu
+    beta.ss[[aspect]][,words] <- phi + beta.ss[[aspect]][,words]
+#     bound[i] <- doc.results$bound
+    lambda[[i]] <- eta
 
     if(verbose && i%%ctevery==0) cat(".")
   }
