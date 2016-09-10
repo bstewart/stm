77a542909bc25341b96c2cf2cec14ce3f734a01c
christopher baker <chris.baker@vt-arc.org>
2016-09-09 22:28:51 -0400

    Add test for plot.STM to ferret out where Travis error is coming from.

---
+++ tests/testthat/test-visualize.R
@@ -0,0 +1,8 @@
+context("Visualize results")
+
+test_that("plot.STM doesn't throw error ", {
+  load(url("http://goo.gl/VPdxlS"))
+  tryCatch(plot.STM(poliblogContent, type = "perspectives", topics = 11),
+           error = function(q) q) -> tca
+  expect_true(!("error" %in% class(tca)))
+})
