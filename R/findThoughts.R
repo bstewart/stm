a4948fa1db56118fb54d7cfd6383485ba76514ab
Brandon Stewart <bms4@soc-macbms4.princeton.edu>
2016-09-17 13:59:53 -0400

    data.table addition to findThoughts

--- R/findThoughts.R
+++ R/findThoughts.R
@@ -1,23 +1,47 @@
-findThoughts <- function(model, texts=NULL, topics=NULL, n=3, thresh=0.0){
-  #Grab up to n texts which are above the threshold
+findThoughts <- function(model, texts=NULL, topics=NULL, n=3, thresh=NULL,
+                         where=NULL, meta=NULL) {
   theta <- model$theta
   if(is.null(topics)) topics <- 1:ncol(theta)
   if(!is.null(texts) && length(texts)!=nrow(theta)) stop("Number of provided texts and number of documents modeled do not match")
   #since we are allowing n to be infinity have to recode if its too high.
   if(n > nrow(theta)) n <- nrow(theta)
   if(n < 1) stop("Must request at least one returned document.")
-  
   out <- list()
-  for(i in 1:length(topics)) {
-    k <- topics[i]
-    #grab the values and the rank
-    index <- order(theta[,k], decreasing=TRUE)[1:n]
-    val <- sort(theta[,k], decreasing=TRUE)[1:n]
-    #subset to those values which meet the threshold
-    index <- index[which(val>=thresh)]
-    out$index[[i]] <- index
-    #grab the associated texts
-    if(!is.null(texts)) out$docs[[i]] <- texts[index]
+  where <- substitute(where)
+
+    #First check if they are trying to use thresh and where together
+  if(!is.null(where) & !is.null(thresh)) warning("Threshold value is ignored when where argument is non null \n Include threshold explicitly in where statement.")
+  #okay now if thresh isn't used fill it in as 0
+  if(is.null(thresh)) thresh <- 0.0
+  
+  #If they are using "where" we need the data.table infrastructure
+  if(!is.null(where)) {
+    colnames(theta) <- sprintf("Topic%i", 1:ncol(theta))
+    if(is.null(meta)) {
+      dt <- data.table(docnum=1:nrow(theta),theta)
+    } else {
+      dt <- data.table(docnum=1:nrow(theta),theta, meta)
+    }
+    for(i in 1:length(topics)) {
+      what <- parse(text=sprintf("docnum[order(Topic%i, decreasing=TRUE)][1:%i]", topics[i], n))
+      index <- dt[eval(where), eval(what)]
+      out$index[[i]] <- index
+      #grab the associated texts
+      if(!is.null(texts)) out$docs[[i]] <- texts[index]
+    }
+  } else {
+    #Here we are in the simpler non-"where" branch of the tree
+    for(i in 1:length(topics)) {
+      k <- topics[i]
+      #grab the values and the rank
+      index <- order(theta[,k], decreasing=TRUE)[1:n]
+      val <- sort(theta[,k], decreasing=TRUE)[1:n]
+      #subset to those values which meet the threshold
+      index <- index[which(val>=thresh)]
+      out$index[[i]] <- index
+      #grab the associated texts
+      if(!is.null(texts)) out$docs[[i]] <- texts[index]
+    }
   }
   names(out$index) <- paste("Topic", topics)
   if(!is.null(texts)) names(out$docs) <- paste("Topic", topics)
@@ -31,7 +55,7 @@ findThoughts <- function(model, texts=NULL, topics=NULL, n=3, thresh=0.0){
     warning("texts are of type 'factor.'  Converting to character vectors.  Use 'as.character' to avoid this warning in the future.")
     out$docs <- lapply(out$docs, as.character)  
   }
-
+  
   return(out)
 }
 
@@ -51,3 +75,13 @@ plot.findThoughts <- function(x, sentences=NULL, ...) {
   }
 }
 
+
+make.dt <- function(model, meta=NULL) {
+  theta <- model$theta
+  colnames(theta) <- sprintf("Topic%i", 1:ncol(theta))
+  if(is.null(meta)) {
+    return(data.table::data.table(docnum=1:nrow(theta),theta))
+  } else {
+    return(data.table::data.table(docnum=1:nrow(theta),theta, meta))
+  }
+}
