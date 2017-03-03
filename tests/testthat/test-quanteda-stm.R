ae6b092e03e3e4f72523fe587bf043a0a7540409
Brandon Stewart <bms4@soc-macbms4.princeton.edu>
2017-03-03 11:04:18 -0500

    Adding condition to tm test

--- tests/testthat/test-quanteda-stm.R
+++ tests/testthat/test-quanteda-stm.R
@@ -22,23 +22,24 @@ test_that("Test that stm works on a quanteda dfm", {
   }
 })
 
-test_that("Test that stm works on a classic stm object structure", {
-  temp <- textProcessor(documents = gadarian$open.ended.response,
-                        metadata = gadarian)
-  meta <- temp$meta
-  vocab <- temp$vocab
-  docs <- temp$documents
-  out <- prepDocuments(docs, vocab, meta)
-  docs <- out$documents
-  vocab <- out$vocab
-  meta <- out$meta
-  set.seed(10012)
-  stm_from_stmclassic <- 
-    stm(docs, vocab, 3, prevalence = ~treatment + s(pid_rep), data = meta,
-        max.em.its = 2)
-  expect_identical(class(stm_from_stmclassic), "STM")
-})
-
+if(requireNamespace("tm",quietly=TRUE) & utils::packageVersion("tm")>=0.6) {
+  test_that("Test that stm works on a classic stm object structure", {
+    temp <- textProcessor(documents = gadarian$open.ended.response,
+                          metadata = gadarian)
+    meta <- temp$meta
+    vocab <- temp$vocab
+    docs <- temp$documents
+    out <- prepDocuments(docs, vocab, meta)
+    docs <- out$documents
+    vocab <- out$vocab
+    meta <- out$meta
+    set.seed(10012)
+    stm_from_stmclassic <- 
+      stm(docs, vocab, 3, prevalence = ~treatment + s(pid_rep), data = meta,
+          max.em.its = 2)
+    expect_identical(class(stm_from_stmclassic), "STM")
+  })
+}
 ## could add an additional test to compare the two outputs
 ## could differ based on tokenizer, although
 ## tm::stopwords("english") is the same set as quanteda::stopwords("english")
