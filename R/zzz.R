672d6904627d8afe132314caa1c9a2d4da77999b
Brandon Stewart <bms4@soc-macbms4.princeton.edu>
2018-01-27 14:26:18 -0500

    Final tweaks for submitting to CRAN

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   pd <- utils::packageDescription(pkgname);
   packageStartupMessage(pkgname, " v", pd$Version, " (",
-                    pd$Date, ") successfully loaded. See ?", pkgname, " for help.");
+                    pd$Date, ") successfully loaded. See ?", pkgname, " for help. \n Papers, resources, and other materials at structuraltopicmodel.com");
 }
