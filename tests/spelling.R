70f1d119cdd7ff90bb9936996f14be70929f0f89
kbenoit <kbenoit@lse.ac.uk>
2018-12-10 00:40:05 +1100

    Configure automatic spell checking

---
+++ tests/spelling.R
@@ -0,0 +1,4 @@
+if (requireNamespace("spelling", quietly = TRUE))
+  spelling::spell_check_test(vignettes = TRUE, 
+                             error = TRUE,
+                             skip_on_cran = TRUE)
