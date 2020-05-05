4e7f0eff62267f41e94bfc05965933c38b2ae68e
Gyorgy Matyasfalvi <matyasfalvi@princeton.edu>
2020-05-05 17:07:30 -0400

    G.M. -- Updated basic_test.R.

--- sprint/basic_test.R
+++ sprint/basic_test.R
@@ -1,6 +1,6 @@
 library(stm)
 
-detercpp <- stm(poliblog5k.docs, poliblog5k.voc, K=3, prevalence=~rating, data=poliblog5k.meta, max.em.its = 100, control=list(method="BFGS", neum_sum_cpp=TRUE))
+detercpp <- stm(poliblog5k.docs, poliblog5k.voc, K=40, prevalence=~rating, data=poliblog5k.meta, max.em.its = 100, control=list(method="BFGS", neum_sum_cpp=TRUE))
 
 #deter <- stm(poliblog5k.docs, poliblog5k.voc, K=40, prevalence=~rating, data=poliblog5k.meta, max.em.its = 100, control=list(method="BFGS"))
 #randcpp <- stm(poliblog5k.docs, poliblog5k.voc, K=10, prevalence=~rating, data=poliblog5k.meta, seed=13, max.em.its = 100, control=list(method="BFGS", rand_docs=TRUE, neum_sum_cpp=TRUE))
