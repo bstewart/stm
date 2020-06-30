70dadfe181808d330aea4899923a455fec16a65b
Gyorgy Matyasfalvi <matyasfalvi@princeton.edu>
2020-06-30 15:08:54 -0400

    Fixed stab_test.R

--- inst/stab_test.R
+++ inst/stab_test.R
@@ -5,17 +5,12 @@ library(stringr)
 # Get command line argument for operating system
 args = commandArgs(trailingOnly=TRUE)
 
-# Generate data
-docs = poliblog5k.docs
-voc = poliblog5k.voc
-heldout <- make.heldout(docs, voc)
-
-reg <- stm(heldout$documents, heldout$vocab, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="BFGS"))
+reg <- stm(poliblog5k.docs, poliblog5k.voc, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="BFGS"))
 
 reg_file = str_c("reg_", args[1], ".Rda")
 save(reg, file=reg_file)
 
-neum <- stm(heldout$documents, heldout$vocab, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="BFGS", neum_sum_cpp=TRUE))
+neum <- stm(poliblog5k.docs, poliblog5k.voc, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="BFGS", neum_sum_cpp=TRUE))
 
 neum_file = str_c("neum_", args[1], ".Rda")
 save(neum, file=neum_file)
