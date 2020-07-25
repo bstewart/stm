d614e5844602ebe1f109ad829fd2c1a757576a9e
Gyorgy Matyasfalvi <matyasfalvi@princeton.edu>
2020-07-25 19:42:24 -0400

    Updated cross platform test script.

--- inst/xpl_stab_test.R
+++ inst/xpl_stab_test.R
@@ -6,10 +6,10 @@ args = commandArgs(trailingOnly=TRUE)
 file_names = c("./artifact/reg_oneLinux.Rds", "./artifact/reg_onemacOS.Rds", "./artifact/reg_oneWindows.Rds", "./artifact/neum_oneLinux.Rds", "./artifact/neum_onemacOS.Rds", "./artifact/neum_oneWindows.Rds")
 
 for(i in 1:length(file_names)) {
-  if(file_names[i] == "reg_oneLinux.Rds") {
+  if(file_names[i] == "./artifact/reg_oneLinux.Rds") {
     reg_oneLinux <- readRDS(file_names[i])
   }
-  else if(file_names[i] == "neum_onemacOS.Rds") {
+  else if(file_names[i] == "./artifact/neum_onemacOS.Rds") {
     neum_onemacOS <- readRDS(file_names[i])
   }
 }
