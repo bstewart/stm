eacf28a967daf9013b99d03573c3816565f1d92b
Gyorgy Matyasfalvi <matyasfalvi@princeton.edu>
2020-07-26 12:18:48 -0400

    Updated cross platform tests.

--- inst/xpl_stab_test.R
+++ inst/xpl_stab_test.R
@@ -3,6 +3,8 @@
 # Get command line argument for operating system
 args = commandArgs(trailingOnly=TRUE)
 
+print(getwd())
+
 file_names = c("../artifact/reg_Linux.Rds", "../artifact/reg_macOS.Rds", "../artifact/reg_Windows.Rds", "../artifact/neum_Linux.Rds", "../artifact/neum_macOS.Rds", "../artifact/neum_Windows.Rds")
 
 for(i in 1:length(file_names)) {
