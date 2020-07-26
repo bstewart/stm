90a564f2f8305b058f05b813ddf4700a71eef4d8
Gyorgy Matyasfalvi <matyasfalvi@princeton.edu>
2020-07-25 21:15:50 -0400

    Updated cross platform tests.

--- inst/xpl_stab_test.R
+++ inst/xpl_stab_test.R
@@ -3,15 +3,15 @@
 # Get command line argument for operating system
 args = commandArgs(trailingOnly=TRUE)
 
-file_names = c("../artifact/reg_oneLinux.Rds", "../artifact/reg_onemacOS.Rds", "../artifact/reg_oneWindows.Rds", "../artifact/neum_oneLinux.Rds", "../artifact/neum_onemacOS.Rds", "../artifact/neum_oneWindows.Rds")
+file_names = c("../artifact/reg_Linux.Rds", "../artifact/reg_macOS.Rds", "../artifact/reg_Windows.Rds", "../artifact/neum_Linux.Rds", "../artifact/neum_macOS.Rds", "../artifact/neum_Windows.Rds")
 
 for(i in 1:length(file_names)) {
-  if(file_names[i] == "../artifact/reg_oneLinux.Rds") {
-    reg_oneLinux <- readRDS(file_names[i])
+  if(file_names[i] == "../artifact/reg_Linux.Rds") {
+    reg_Linux <- readRDS(file_names[i])
   }
-  else if(file_names[i] == "../artifact/neum_onemacOS.Rds") {
-    neum_onemacOS <- readRDS(file_names[i])
+  else if(file_names[i] == "../artifact/neum_macOS.Rds") {
+    neum_macOS <- readRDS(file_names[i])
   }
 }
 
-print(all.equal(reg_oneLinux, neum_onemacOS, tolerance=1.0e-13))
+print(all.equal(reg_Linux, neum_macOS, tolerance=1.0e-13))
