13f4ba10cefe48084162ac31e05704e50bff07e2
Gyorgy Matyasfalvi <matyasfalvi@princeton.edu>
2020-07-28 13:46:19 -0400

    Updated tests to use CG.

--- inst/reg_neum_run.R
+++ inst/reg_neum_run.R
@@ -6,12 +6,12 @@ library(stringr)
 args = commandArgs(trailingOnly=TRUE)
 
 set.seed(13)
-reg <- stm(poliblog5k.docs, poliblog5k.voc, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="L-BFGS-B"))
+reg <- stm(poliblog5k.docs, poliblog5k.voc, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="CG"))
 reg_file = str_c("reg_", args[1], ".Rds")
 saveRDS(reg, file=reg_file)
 
 set.seed(13)
-neum <- stm(poliblog5k.docs, poliblog5k.voc, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="L-BFGS-B", neum_sum_cpp=TRUE))
+neum <- stm(poliblog5k.docs, poliblog5k.voc, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="CG", neum_sum_cpp=TRUE))
 neum_file = str_c("neum_", args[1], ".Rds")
 saveRDS(neum, file=neum_file)
 
