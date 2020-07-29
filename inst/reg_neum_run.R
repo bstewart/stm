5d0b6b9554d082f671b0f001568c947e736b8a1b
Gyorgy Matyasfalvi <matyasfalvi@princeton.edu>
2020-07-29 19:32:32 -0400

    Test with optimr (BFGS).

--- inst/reg_neum_run.R
+++ inst/reg_neum_run.R
@@ -6,12 +6,12 @@ library(stringr)
 args = commandArgs(trailingOnly=TRUE)
 
 set.seed(13)
-reg <- stm(poliblog5k.docs, poliblog5k.voc, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="nlm"))
+reg <- stm(poliblog5k.docs, poliblog5k.voc, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="BFGS"))
 reg_file = str_c("reg_", args[1], ".Rds")
 saveRDS(reg, file=reg_file)
 
 set.seed(13)
-neum <- stm(poliblog5k.docs, poliblog5k.voc, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="nlm", neum_sum_cpp=TRUE))
+neum <- stm(poliblog5k.docs, poliblog5k.voc, K=5, init.type = "Spectral", max.em.its = 100, control=list(method="BFGS", neum_sum_cpp=TRUE))
 neum_file = str_c("neum_", args[1], ".Rds")
 saveRDS(neum, file=neum_file)
 
