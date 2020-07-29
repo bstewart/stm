98e7f83ac48737d87242c153cb154ea36ee0e399
Gyorgy Matyasfalvi <matyasfalvi@princeton.edu>
2020-07-29 15:26:46 -0400

    Post proc (50) test with optim.

--- R/STMlncpp.R
+++ R/STMlncpp.R
@@ -24,7 +24,7 @@ logisticnormalcpp <- function(eta, mu, siginv, beta, doc, sigmaentropy,
   #                    doc_ct=doc.ct, mu=mu,
   #                    siginv=siginv, beta=beta)
   
-  optim.out <- optim(par=optim.out$par, fn=lhoodcpp, gr=gradcpp,
+  optim.out <- optim(par=eta, fn=lhoodcpp, gr=gradcpp,
                      method=method, control=control,
                      doc_ct=doc.ct, mu=mu,
                      siginv=siginv, beta=beta)
