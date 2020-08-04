6ff58392b9fd0acead4c54a5bff39d3fa4c6731e
Gyorgy Matyasfalvi <matyasfalvi@princeton.edu>
2020-08-04 10:18:54 -0400

    ucminf doc level 1e-6 + val.

--- inst/xpl_opt_test.R
+++ inst/xpl_opt_test.R
@@ -9,7 +9,8 @@ files <- list.files(path = location)
 # Variables
 num_files <- length(files)
 os_names <- character()
-bad_docs <- numeric()
+diff_sol <- numeric()
+diff_val <- numeric()
 
 # Loop through all the file names that have naming convention: 
 # methodType_tolerance_osType.Rds
@@ -30,7 +31,7 @@ os_names <- unique(os_names, MARGIN = 2)
 # Generate all pair combinations of os-s.
 comb_os <- combn(os_names, 2)
 
-# Check equality for all combinations of os-s for each topic
+# Check equality for all combinations of os-s
 for(j in 1:ncol(comb_os)) {
   cat(paste("Comparing", comb_os[1,j], "vs", comb_os[2,j], "at", tolerance, "\n"))
   file_name1 <- str_c(location, m_type, "_", tolerance, "_", comb_os[1,j], ".", f_ext)
@@ -38,13 +39,17 @@ for(j in 1:ncol(comb_os)) {
   os1 <- readRDS(file_name1)
   os2 <- readRDS(file_name2)
   
-  # Search for bad document
+  # Search for different solutions and objective values
   for(i in 1:nrow(os1)) {
     if(!isTRUE(all.equal(os1$sol_norm2[i], os2$sol_norm2[i]))) {
-      bad_docs <- cbind(bad_docs, i)
+      diff_sol <- cbind(diff_sol, i)
+    }
+    if(!isTRUE(all.equal(os1$obj_value[i], os2$obj_value[i]))) {
+      diff_val <- cbind(diff_val, i)
     }
   }
   
-  cat(paste("Number of bad docs:", length(bad_docs), "for method:", m_type, "at", tolerance, ".\n"))
-  #print(bad_docs)
+  cat(paste("Number of different solutions:       ", length(diff_sol), "for method:", m_type, "at", tolerance, ".\n"))
+  cat(paste("Number of different objective values:", length(diff_val), "for method:", m_type, "at", tolerance, ".\n"))
+  #print(diff_sol)
 }
